f8325079710e90fe4ec0bab4dd0bf5ae95fcc9fc
NormaLanc <Norma.a.lancaster@gmail.com>
2026-09-12 14:24:34 -0400

    previous edits from desktop

--- family_saas/lib/Pages/App/settings.dart
+++ family_saas/lib/Pages/App/settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 //The details to send invites will be on this page.
+//TODO: Generate one-time invite codes for inviting new family members
+//TODO: Invite permissions are tied to unique one-time invite codes
+//TODO: Family role is tied to unique one-time invite codes
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -15,6 +18,9 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
+
+      //Invite new family members using one-time invite codes
+     
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
